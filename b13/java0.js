9b6aef00d2a982e3b3d08873cd534439c7a34af7
phannam06 <phanvuhoainam11d1@gmail.com>
2024-07-20 16:25:43 +0700

    Add files via upload

---
+++ b13/java0.js
@@ -0,0 +1,26 @@
+// alert("Học Javasript");
+// confirm("Bạn có chắc chắn không");
+// prompt("Bạn bao nhiêu tuổi ", 20);
+var a = 10;
+var b = 5;
+// document.write("Tổng của hai số a và b là: " + (a + b));
+// document.write("Hiệu của hai số a và b là: " + (a - b));
+// function max(a, b) {
+//   if (a > b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+// document.write("Max là: " + max(a, b));
+// var person = {
+//   name: "Nam",
+//   lastName: "Phan",
+//   age: 20,
+//   email: "namphan06124@gmail.com",
+// };
+// person.email;
+// person.color = "blue";
+// console.log(person);
+// document.write(person.name);
+price = prompt("Enter");
